0ec2a1dc63827a52661d6344734021b751c1bbc2
reignu <ungier.adam@gmail.com>
2024-12-18 15:16:01 +0000

    Added Todo component

--- frontend/src/components/todo/index.js
+++ frontend/src/components/todo/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Container } from './styles'
 
 function Todo() {
     return (
-        <div>Hell this is the todo component</div>
+        <Container><h2>List of Tasks</h2>
+        {/* Form Component */}
+        {/* Task */}
+        {/* Key */}
+        {/* Author */}
+        </Container>
     )
 }
 
